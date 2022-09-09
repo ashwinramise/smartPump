6e9583a5bb93669b3145179590e58a779b52aed7
aramach5 <aramach5@ncsu.edu>
2022-09-09 09:40:53 -0500

    dbServices Added

--- EdgeConnect.py
+++ EdgeConnect.py
@@ -19,8 +19,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+
 
 from pymodbus.pdu import ModbusRequest
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
